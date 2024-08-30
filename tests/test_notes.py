bbd99272ff0ac6887666d71022670f54c1731fdf
Nekr <justnekr@gmail.com>
2024-08-30 14:30:10 +0200

    add test spelling

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -42,3 +42,21 @@ async def test_read_notes(async_client_test: AsyncClient, setup_user):
     assert len(notes) > 0
     assert notes[0]["title"] == "Test Note"
     assert notes[0]["content"] == "This is a test note"
+
+
+async def test_spelling_note(async_client_test: AsyncClient, setup_user):
+    response = await async_client_test.post(
+        "/auth/token/", data={"username": "string", "password": "string"}
+    )
+    token = response.json().get("access_token")
+
+    response = await async_client_test.post(
+        "/notes/",
+        headers={"Authorization": f"Bearer {token}"},
+        json={"title": "Test Note", "content": "Страка слишкам"},
+    )
+
+    assert response.status_code == 200
+    data = response.json()
+    assert data["title"] == "Test Note"
+    assert data["content"] == "Строка слишком"
