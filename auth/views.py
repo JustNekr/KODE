77edbc58ea1c4ea6ba79df73b288c00bed19899f
Nekr <justnekr@gmail.com>
2024-08-30 14:25:51 +0200

    update create_user endpoint

--- auth/views.py
+++ auth/views.py
@@ -4,6 +4,7 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
@@ -47,17 +48,20 @@ async def login(
 
 
 @router.post("/user/", response_model=UserResponse)
-async def create_user(
-    user: UserCreate, session: AsyncSession = Depends(get_async_session)
-):
-    async with session.begin():
-        result = await session.execute(select(User).filter_by(username=user.username))
-        db_user = result.scalars().first()
-        if db_user:
-            raise HTTPException(status_code=400, detail="Username already registered")
-        hashed_password = get_password_hash(user.password)
-        db_user = User(username=user.username, hashed_password=hashed_password)
-        session.add(db_user)
-    await session.commit()
-    await session.refresh(db_user)
-    return db_user
+async def create_user(user: UserCreate, db: AsyncSession = Depends(get_async_session)):
+    try:
+        async with db.begin():
+            result = await db.execute(select(User).filter_by(username=user.username))
+            db_user = result.scalars().first()
+            if db_user:
+                raise HTTPException(
+                    status_code=400, detail="Username already registered"
+                )
+            hashed_password = get_password_hash(user.password)
+            db_user = User(username=user.username, hashed_password=hashed_password)
+            db.add(db_user)
+        await db.commit()
+        await db.refresh(db_user)
+        return db_user
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail="Database error occurred")
